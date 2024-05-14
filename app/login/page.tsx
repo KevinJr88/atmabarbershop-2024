b9a79db93a4482487e1f01c90f8039ccd7aa1673
Kevin <julianrkevin6@gmail.com>
2024-05-14 19:33:13 +0700

    edit login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import Image from 'next/image';
  
+
+
+//kebjkekfjekfnwknfjken ksekfklekfke
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
